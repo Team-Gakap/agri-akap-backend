53270a8b006988f8b0e97b5d5b14ad53a959c219
JohnMitchelDupitas <jmdupitas5@gmail.com>
2026-08-26 16:44:09 +0800

    Remove obsolete seeders from DatabaseSeeder to streamline database initialization

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,20 +6,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-   
+
     public function run(): void
     {
         $this->call([
             BarangayCoordinateSeeder::class,
             UserSeeder::class,
             BarangayUserSeeder::class,
-            ProgramSeeder::class,
-            FarmerSeeder::class,
-            FarmPlotSeeder::class,
-            DamageAssessmentSeeder::class,
-            PestOutbreakSeeder::class,
-            CropMonitoringSeeder::class,
-            ReportWorkflowSeeder::class,
         ]);
     }
 }
